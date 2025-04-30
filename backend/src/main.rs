372000adb229adf7f35a2c01a46b9eb029aee2db
Aiden Gindin <aiden@thegindins.com>
2025-04-30 19:48:06 -0400

    Add basic Axum webserver

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,24 @@
 pub mod domain;
 
-fn main() {
-    println!("Hello world!");
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    Json, Router,
+};
+use serde::{Serialize, Deserialize};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new();
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .expect("Failed to bind TCP listner");
+    axum::serve(listener, app)
+        .await
+        .expect("Server failed");
 }
+
 
